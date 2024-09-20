5f84a4dab09886ed3a2a524bf28989aea7986800
elspaey <m.elspaey3@gmail.com>
2024-09-21 00:22:32 +0300

    createNoteModel

--- lib/views/widgets/editNoteViewBody.dart
+++ lib/views/widgets/editNoteViewBody.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_10/views/widgets/customAppBar.dart';
-import 'package:flutter_application_10/views/widgets/customSearchIcon.dart';
 
 class Editnoteviewbody extends StatelessWidget {
   const Editnoteviewbody({super.key});
