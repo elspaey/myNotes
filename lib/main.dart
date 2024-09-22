84bb05a0e9959d93e52faa3bf3a4b8946abb1de4
elspaey <m.elspaey3@gmail.com>
2024-09-22 19:19:48 +0300

    + add note cubit to add noter buttom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_10/bloc/add_note_cubit/add_note_cubit.dart';
 import 'package:flutter_application_10/helper/constants.dart';
 import 'package:flutter_application_10/views/editNoteView.dart';
 import 'package:flutter_application_10/views/notesViews.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
@@ -15,15 +17,22 @@ class noteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        Editnoteview.id: (context) => const Editnoteview(),
-        Notesviews.id: (context) => const Notesviews()
-      },
-      initialRoute: Notesviews.id,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
-      home: const Notesviews(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        routes: {
+          Editnoteview.id: (context) => const Editnoteview(),
+          Notesviews.id: (context) => const Notesviews()
+        },
+        initialRoute: Notesviews.id,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
+        home: const Notesviews(),
+      ),
     );
   }
 }
--- lib/views/widgets/addNoteButtonSheet.dart
+++ lib/views/widgets/addNoteButtonSheet.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'addNoteForm.dart';
+import 'package:flutter_application_10/bloc/add_note_cubit/add_note_cubit.dart';
+import 'package:flutter_application_10/views/widgets/addNoteForm.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class buttomSheet extends StatelessWidget {
   const buttomSheet({super.key});
@@ -22,7 +24,21 @@ class bodyOfButtonSheet extends StatelessWidget {
         child: Padding(
           padding:
               EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: const addNoteForm(),
+          child: BlocConsumer<AddNoteCubit, AddNoteState>(
+            listener: (context, state) {
+              if (state is AddNoteFailure) {
+                print("failied${state.errorMessage}");
+              }
+              if (state is AddNoteSuccess) {
+                Navigator.pop(context);
+              }
+            },
+            builder: (context, state) {
+              return ModalProgressHUD(
+                  inAsyncCall: state is AddNoteLoading ? true : false,
+                  child: const addNoteForm());
+            },
+          ),
         ),
       ),
     );
